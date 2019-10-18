d57735aea4f89f3f11dd86786269988a5a336d85
Adejobi Ezekiel <ezeko2017@gmail.com>
2019-10-18 22:04:39 +0100

    add cors error prevention

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express'); //import express
 const bodyParser = require('body-parser'); // import body-parser
 const app = express();
 
+//cross origin resource sharing error prevention below
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 app.use(bodyParser.json()); //call body-parser
 app.use((req,res, next)=>{
