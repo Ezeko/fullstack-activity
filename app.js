ac396c23e3f6ab4fcbbb98ffd790976d5e68e36b
Adejobi Ezekiel <ezeko2017@gmail.com>
2019-10-18 22:01:49 +0100

    configure body-parser

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express'); //import express
-
+const bodyParser = require('body-parser'); // import body-parser
 const app = express();
 
+
+app.use(bodyParser.json()); //call body-parser
 app.use((req,res, next)=>{
     res.end(console.log('connected to server'))
 })
