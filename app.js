65eeec0e9ff3559dcf66244a9360df1f67ef1fd0
Adejobi Ezekiel <ezeko2017@gmail.com>
2019-10-18 22:38:00 +0100

    add update route

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.post('/api/recipes',(req,res,next)=>{
             difficulty: req.body.difficulty,
             time: req.body.time
            
-    })
+    });
 
     recipe.save().then(()=>{
         res.status(201).json({
@@ -51,12 +51,35 @@ app.post('/api/recipes',(req,res,next)=>{
 app.get('/api/recipes/:id', (req, res, next)=>{
     Recipe.findOne({_id: req.params.id}).then((recipe)=>{
         res.status(200).json(recipe);
+    }).catch((error)=>{
+        res.status(404).json({
+            error: error
+        })
     })
     next();
 });
 
 //Update route
 app.put('/api/recipes/:id', (req, res, next)=>{
+    recipe = new Recipe({
+        _id: req.params.id,
+        title: req.body.title,
+        ingredients: req.body.ingredients,
+        instructions: req.body.instructions,
+        difficulty: req.body.difficulty,
+        time: req.body.time
+       });
+
+       Recipe.updateOne({_id: req.params.id}, recipe).then(()=>{
+           res.status(200).json({
+               message: "Recipe updated Successfully"
+           })
+       }).catch((error)=>{
+           res.status(400).json({
+               error: error
+           })
+       });
+       next();
 
 })
 //get route
